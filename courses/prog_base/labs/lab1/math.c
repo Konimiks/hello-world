90a015bd3cbe47ce0af52ae39fba1e47999abf20
Konimiks <dyachenko.dmtr@gmail.com>
2015-10-08 18:46:57 +0300

    2

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -5,20 +5,14 @@ double calc(double x, double y, double z)  {
 	double a;
 	double a0,a1,a2,g,j;
 	
-	if (sin(y)<0) g=-sin(y);
-	else g=sin(y);
-	if ((x+y)<0) j=-(x+y);
-	else j=(x+y);
 	
-	
-	if(sin(y)==0 || z==0 || (x-y)<=0 || (x+y)==0 || (x-y)==0 || x<0 ) return NAN;
-	
-	
+	if(sin(y)==0 || z==0 || (x-y)<=0 || (x+y)==0 || (x-y)==0 || x<0 ) a=NAN;
+	else{	
    	a0=pow(x,y+1)/pow(x-y,1/z);
-	a1=y/(4*j);
-	a2=pow(x,1/g);
+  	a1=y/(4*fabs(x+y));
+	a2=pow(x,1/fabs(sin(y)));
 	
 	a=a0+a1+a2;
 
-	return a;
+	return a;}
 }
