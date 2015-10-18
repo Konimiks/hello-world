90c2f70614abb689676e425025bb7caeef0e8df1
Konimiks <dyachenko.dmtr@gmail.com>
2015-10-18 21:31:21 +0300

    45

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -4,7 +4,6 @@
 double long2double(long long b)
 {
 
-long long b;
 short znak, i;
 double Man, result;
 int E;
@@ -19,14 +18,14 @@ E = abs((b>>52)+1);
 for (i = 51; i >= 0; --i) {
 Man+=(((b>>i)&1)*(pow(2, i - 52)));
 }
-if ((E == 2047) && (Man!=0.0)) return printf ("result = NaN");
-if ((E == 2047) && (Man == 0.0) && (znak == 1) )return printf ("result = -Infinity");
-if ((E == 2047) && (Man == 0.0) && (znak == 0))return printf ("result = Infinity");
+if ((E == 2047) && (Man!=0.0)) return ;
+if ((E == 2047) && (Man == 0.0) && (znak == 1) )return -INFINITY;
+if ((E == 2047) && (Man == 0.0) && (znak == 0))return INFINITY;
 if ((E > 0) && (E <2047)) {result = (pow(-1,znak)* pow(2,E-1023)*(1+Man));
 printf("%.20llf", result);}
 if ((E == 0) && (Man !=0.0)) {result = (pow(-1, znak)* pow(2, -1022)*(Man));
 printf("%.325llf", result);}
-if ((E == 0) && (Man == 0.0) && (znak == 1))return printf("result = -0");
-if ((E == 0) && (Man == 0.0) && (znak == 0)) return printf ("result = 0");
+if ((E == 0) && (Man == 0.0) && (znak == 1))return printf("-0");
+if ((E == 0) && (Man == 0.0) && (znak == 0)) return printf ("0");
 
 }
