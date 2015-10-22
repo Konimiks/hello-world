676f1d26210f04207da9b91dcfb703131ebd513f
Konimiks <dyachenko.dmtr@gmail.com>
2015-10-22 23:19:10 +0300

    ready

---
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void fillRand(int mat[4][4]){
+    int i,j;
+    srand(time(0));
+    for (i=0; i<4; i++)  {
+        for (j=0; j<4; j++)  {
+            mat [i][j] = rand() % 1999-999 ;
+
+        }
+    }
+}
+
+void rotateCCW90(int mat[4][4]){
+    int i,j;
+    int mat2 [4][4];
+    for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat2[i][j]=mat[i][j];
+    for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat[i][j]=mat2[j][3-i];
+}
+
+
+void flipH(int mat[4][4]) {
+ int i,j;
+ int mat2 [4][4];
+ for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat2[i][j]=mat[i][j];
+    for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat[i][j]=mat2[j][i];
+}
+
+
+void transposSide(int mat[4][4]){
+int i,j;
+ int mat2 [4][4];
+ for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat2[i][j]=mat[i][j];
+    for(i=0; i<4; i++)
+        for(j=0;j<4;j++)
+            mat[i][j]=mat2[3-j][i];
+}
