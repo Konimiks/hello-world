05c921ebd971a54f39c6671daa8c9c13ba168210
Konimiks <dyachenko.dmtr@gmail.com>
2015-10-26 15:31:16 +0300

    new

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,31 +1,49 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
 
-double long2double(long long b)
-{
+double long2double(long long number) {
 
-short znak, i;
-double Man=0.0, result;
-int E;
-if ((b>>63)==0){
-znak = 0;
-E = (b>>52);
-}
-else {
-znak=1;
-E = abs((b>>52)+1);
-}
-for (i = 51; i >= 0; --i) {
-Man+=(((b>>i)&1)*(pow(2, i - 52)));
-}
-if ((E == 2047) && (Man!=0.0)) return NAN;
-if ((E == 2047) && (Man == 0.0) && (znak == 1) )return -INFINITY;
-if ((E == 2047) && (Man == 0.0) && (znak == 0))return INFINITY;
-if ((E > 0) && (E <2047)) {result = (pow(-1,znak)* pow(2,E-1023)*(1+Man));
-return result;}
-if ((E == 0) && (Man !=0.0)) {result = (pow(-1, znak)* pow(2, -1022)*(Man));
-return result;}
-if ((E == 0) && (Man == 0.0) && (znak == 1)) return -0.0;
-if ((E == 0) && (Man == 0.0) && (znak == 0)) return 0;
+	int arr[64];
+	double exp[11], man[52];
+	double fract = 0, expn = 0, sign;
+	int i;
+
+	for (i = 0; i <= 63; i++) {
+		arr[i] = (number & (1LL << 63 - i)) ? 1 : 0;
+	}
+	for (i = 0; i < 11; i++) {
+		exp[i] = arr[i + 1];
+	}
+	for (i = 0; i < 52; i++) {
+		man[i] = arr[i + 12];
+	}
+	sign = arr[0];
+	for (i = 0; i <= 10; i++) {
+		expn += exp[i] * pow(2, i);
+	}
+	for (i = 0; i < 52; i++) {
+		fract += man[i] * pow(2, -i - 1);
+	}
+	if (expn == 0 && fract != 0) {
+		return pow(-1, sign) * pow(2, -1022) * fract;
+	}
+	if (expn >0 && expn < 2047) {
+		return pow(-1, sign) * pow(2, expn - 1023) * (fract + 1);
+	}
+	if (expn == 2047 && fract != 0) {
+		return NAN;
+	}
+	if (expn == 2047 && fract == 0 && sign == 1) {
+		return -INFINITY;
+	}
+	if (expn == 2047 && fract == 0 && sign == 0) {
+		return INFINITY;
+	}
+	if (expn == 0 && fract == 0 && sign == 1) {
+		return -0;
+	}
+	if (expn == 0 && fract == 0 && sign == 0) {
+		return 0;
+	}
 
 }
