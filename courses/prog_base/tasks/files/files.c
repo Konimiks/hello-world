a266f7c4392c428960d3f73402c3e950c0e0f031
Konimiks <dyachenko.dmtr@gmail.com>
2015-11-29 21:32:38 +0200

    redone

--- courses/prog_base/tasks/files/files.c
+++ courses/prog_base/tasks/files/files.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 void fprocess(const char * pread, const char * pwrite){
     FILE* fr = fopen(pread, "r");
 	FILE* fw = fopen(pwrite, "w");
@@ -11,38 +12,38 @@ void fprocess(const char * pread, const char * pwrite){
 		fgets(str, 1000, fr);
 	}
 	int wordsnumber = 0;
-	//printf("%s\n", str);
 	for(int j = 0; j < 1000; j++){
+        if(j == 0 && str[j] == 32) wordsnumber--;
         if(str[j] == 32 && str[j + 1] != 32) wordsnumber++;
         if(str[j] == 13) break;
 	}
 	wordsnumber++;
-	//printf("%i\n", wordsnumber);
 	int n = 0;
 	int wordslen[wordsnumber];
 	for(int i = 0; i < wordsnumber; i++){
             wordslen[i] = 0;
             for(int j = 0 + n; j < 1000; j++){
-                if(str[j] == 32 && str[j + 1] == 32){
-                    n = j + 1;
-                    continue;
+                if(str[j] == 32 || str[j] == 9){
+                        if(str[j + 1] == 32 || str[j] == 9){
+                            n = j + 1;
+                            continue;
+                }
                 }
                 if(str[j] == '\0'){
                     wordslen[i]--;
-                    //printf("%i, ", wordslen[i]);
                     break;
                 }
-                if(str[j] != 32)
+                if(str[j] != 32 && str[j] != 9)
                     wordslen[i]++;
-                if(str[j] == 32 && str[j + 1] != 32){
-                    //printf("%i, ", wordslen[i]);
-                    n = j + 1;
-                    break;
+                if(j != 0 )
+                    if(str[j] == 32 || str[j] == 9)
+                        if(str[j - 1] != 32 || str[j - 1] != 9){
+                        n = j + 1;
+                        break;
                 }
         }
 	}
-	//for(int i = 0; i < wordsnumber; i++)
-    //    fprintf(fw, "%i, ", wordslen[i]);
+
 	fclose(fr);
 	fclose(fw);
 }
