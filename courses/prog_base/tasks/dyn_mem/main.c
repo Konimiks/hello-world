946c882aaef43b74bf087ba89b2277481d7369f2
Konimiks <dyachenko.dmtr@gmail.com>
2015-12-06 12:53:50 +0200

    ready

---
+++ courses/prog_base/tasks/dyn_mem/main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char isCapital(char * string);
+
+int main(void) {
+
+    char * string = NULL;
+    string = malloc(sizeof (char) * 50);
+    if(NULL == string)
+    {
+        printf("NO MEMORY");
+        return 1;
+    }
+    printf("Enter your string (not more than 49 symbols and English)::");
+    gets(string);
+    printf("%s\n", string);
+    printf("%c", isCapital(string));
+    return 0;
+}
+
+char isCapital(char * string)
+{
+    char * number = NULL;
+    number = malloc(sizeof(char) * 1);
+    if(NULL == number)
+        return EXIT_FAILURE;
+    number = '0';
+    for(int i = 0; i <= 49; i++)
+    {
+        if(string[i] <= 'Z' && string[i] >= 'A')
+            number = string[i];
+        if(string[i] == '\0')
+            return number;
+    }
+}
