2b2c2eea011506b21be684e2e11e877a95064e44
Konimiks <dyachenko.dmtr@gmail.com>
2015-12-13 17:07:43 +0200

    ready

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 char elemfirst(char * str, char * p);
-int elem (char * str, char * p, char word);
 
 int main(void){
     printf("Please enter your string:::");
     char string[20];
     gets(string);
     char * p = string;
+
     printf("%c", elemfirst(string, p));
 
     return 0;
@@ -16,32 +16,10 @@ int main(void){
 
 char elemfirst(char * str, char * p)
 {
-    char symbol = * p;
-    int status = elem(str, p, symbol);
-    if( *p == '\0' && status == 0)
+    if(*p == *(p + 1))
+        return *p;
+    else if(*p == '\0')
         return '0';
-    if( status == 1)
-        return symbol;
-    else {
-            p++;
-            elemfirst(str, p);
-    }
+    else elemfirst(str, p + 1);
 
-}
-
-
-int elem (char * str, char * p, char word){
-    char* c = p;
-    p++;
-    if( (*p) == '\0'){
-        p = c;
-        return 0;
-    }
-    if( *p == word){
-        p = c;
-        return 1;
-}
-    else {
-            elem(str, p, word);
-    }
 }
